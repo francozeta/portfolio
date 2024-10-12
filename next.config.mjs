ba322db46e7a9e1be32ed4744dc519342a3fa730
Franco Zeta <perufra@hotmail.com>
2024-10-11 22:29:16 -0500

    Refactor eslint configuration and disable explicit any rule

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  typescript: {
+/*   output: 'export', */
+  eslint: {
     ignoreBuildErrors: true
   }
 };
