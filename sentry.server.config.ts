f65587f9de1108f0a9f463eacf04794003c08b60
Franco Zeta <perufra@hotmail.com>
2024-10-11 19:46:21 -0500

    Add Sentry error monitoring for API route and frontend page

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://150115e6ebfc41ae7596735ec8c5c87e@o4508104492711936.ingest.us.sentry.io/4508104499134464",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
