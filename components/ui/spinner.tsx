c8bc61a09860182122226f1fc34a54dc7bda8858
francozeta <perufra@hotmail.com>
2026-04-26 18:42:43 -0500

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- components/ui/spinner.tsx
+++ components/ui/spinner.tsx
@@ -1,63 +1,16 @@
-"use client"
+import { Loader2Icon } from "lucide-react"
 
-import * as React from "react"
-import { Slot } from "radix-ui"
-import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
-const spinnerVariants = cva("relative block opacity-[0.65]", {
-  variants: {
-    size: {
-      sm: "w-4 h-4",
-      md: "w-6 h-6",
-      lg: "w-8 h-8",
-      xl: "w-12 h-12",
-      "2xl": "w-16 h-16",
-    },
-  },
-  defaultVariants: {
-    size: "sm",
-  },
-})
-
-export interface SpinnerProps extends React.HTMLAttributes<HTMLSpanElement>, VariantProps<typeof spinnerVariants> {
-  loading?: boolean
-  asChild?: boolean
+function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+  return (
+    <Loader2Icon
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
+  )
 }
 
-const Spinner = React.forwardRef<HTMLSpanElement, SpinnerProps>(
-  ({ className, size, loading = true, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot.Root : "span"
-
-    const [bgColorClass, filteredClassName] = React.useMemo(() => {
-      const bgClass = className?.match(/(?:dark:bg-|bg-)[a-zA-Z0-9-]+/g) || []
-      const filteredClasses = className?.replace(/(?:dark:bg-|bg-)[a-zA-Z0-9-]+/g, "").trim()
-      return [bgClass, filteredClasses]
-    }, [className])
-
-    if (!loading) return null
-
-    return (
-      <Comp className={cn(spinnerVariants({ size, className: filteredClassName }))} ref={ref} {...props}>
-        {Array.from({ length: 8 }).map((_, i) => (
-          <span
-            key={i}
-            className="absolute top-0 left-1/2 w-[12.5%] h-full animate-spinner-leaf-fade"
-            style={{
-              transform: `rotate(${i * 45}deg)`,
-              animationDelay: `${-(7 - i) * 100}ms`,
-            }}
-          >
-            <span
-              className={cn("block w-full h-[30%] rounded-full", bgColorClass.length > 0 ? bgColorClass : "bg-white")}
-            ></span>
-          </span>
-        ))}
-      </Comp>
-    )
-  },
-)
-
-Spinner.displayName = "Spinner"
-
-export { Spinner, spinnerVariants }
+export { Spinner }
